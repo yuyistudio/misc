f204fb5a555db56a95192e78df72646b665428b9
Leon of Win7 <1819979772@qq.com>
2016-10-17 00:18:47 +0800

    generate hdr png file

--- pil/hdr.py
+++ pil/hdr.py
@@ -15,51 +15,62 @@ class MyGaussianBlur(ImageFilter.Filter):
         else:
             return image.gaussian_blur(self.radius)
 
-simg = 'example.png'
-dimg = 'demo_blur.png'
-image = Image.open(simg)
-size = image.size
-blur_pixels = 20
-new_size = (blur_pixels * 2 + size[0], blur_pixels * 2 + size[1])
-image_blur = Image.new('RGBA', new_size)
-image_blur.paste(image, (blur_pixels, blur_pixels), image)
-result_image = image_blur.copy()
-bounding = (0,0,new_size[0], new_size[1])
-image_blur = image_blur.filter(MyGaussianBlur(radius=blur_pixels, bounds=bounding))
-image_blur_little = image_blur.filter(MyGaussianBlur(radius=2, bounds=bounding))
+def GenerateHDRImage(input_filename, output_filename):
+    image = Image.open(input_filename)
+    size = image.size
+    blur_pixels = int(size[0] * 0.5)
+    new_size = (blur_pixels * 4 + size[0], blur_pixels * 4 + size[1])
+    image_blur = Image.new('RGBA', new_size)
+    image_blur.paste(image, (blur_pixels * 2, 2 * blur_pixels), image)
+    result_image = image_blur.copy()
+    bounding = (0, 0, new_size[0], new_size[1])
+
+    image_blur_little = image_blur.copy().filter(MyGaussianBlur(radius=5, bounds=bounding))
+    image_blur_big = image_blur.copy().filter(MyGaussianBlur(radius=int(blur_pixels), bounds=bounding))
 
 
-import math
-def merge_blur(*imgs):
-    blur_factor = 1.0
-    size = len(imgs)
-    pixels = [img.load() for img in imgs]
-    def trans(c, s):
-        s += 100
-        s = min(255, s)
-        f = (math.cos(s / 255 * 2 * math.pi) + 1) * 0.5
-        return [f * v for v in c]
-    for x in range(imgs[0].size[0]):
-        for y in range(imgs[0].size[1]):
-            colors = [pixel[x, y] for pixel in pixels]
-            c = [0,0,0]
-            for i in range(3):
-                c[i] = colors[0][i] * 0.2 + colors[1][i] * blur_factor + colors[2][i] * blur_factor
-            '''
-            strength = sum(c) / 3.
-            c = trans(c, strength)
-            '''
-            c.append(255)
-            pixels[0][x, y] = tuple([int(v) for v in c])
-merge_blur(result_image, image_blur, image_blur_little)
-#result_image = Image.blend(result_image, image_blur, 1)
-image_blur.save('blur.png')
-result_image.save(dimg)
-'''
-bounding = (blur_pixels, blur_pixels, blur_pixels + size[0], blur_pixels + size[1])
-image = image.resize(new_size)
-print new_size
-image = image.filter(MyGaussianBlur(radius=blur_pixels, bounds=bounding))
-image.save(dimg)
-print dimg, 'success'
-'''
+    import math
+    def merge_blur(*imgs):
+        factors_common = [2, 2.5, 2.5]
+        factors_diff = [0, 15, 15]
+        size = len(imgs)
+        pixels = [img.load() for img in imgs]
+        def trans(c):
+            # clamp
+            c = [max(0,min(255,v)) for v in c]
+            # get factor
+            f = sum(c) / 3
+            f = (math.cos(f / 255 * 2 * math.pi) + 1)
+            f *= f
+            f *= 0.25
+            # return
+            res = [c[0]*f, c[1]*f, c[2]*f, c[3]]
+            return [int(v) for v in res]
+        for x in range(imgs[0].size[0]):
+            for y in range(imgs[0].size[1]):
+                colors = [pixel[x, y] for pixel in pixels]
+                c = []
+                alpha_origin = colors[0][3]
+                if alpha_origin > 100:# 重叠部分
+                    factors = factors_common
+                else:# 辉光部分
+                    factors = factors_diff
+                for i in range(3):
+                    v = 0
+                    for k in range(len(factors)):
+                        v += colors[k][i] * factors[k]
+                    c.append(int(v * 0.33))
+                if alpha_origin > 100:
+                    alpha = alpha_origin
+                else:
+                    alpha = sum(c) / 3 * 1.5
+
+                #alpha = 255
+
+                c.append(int(alpha))
+                pixels[0][x, y] = tuple(trans(c))
+
+    merge_blur(result_image, image_blur_big, image_blur_little)
+    result_image.save(output_filename)
+
+GenerateHDRImage('input.png', 'output.png')
